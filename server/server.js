99798de644cbaca1e965ac5ba3630fdb8293d341
Ali Usman <ali.usman@indusvalleylabs.com>
2019-02-27 19:06:59 +0500

    Created server.js file

---
+++ server/server.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+	text: {
+		type: String
+	},
+	completed: {
+		type: Boolean
+	},
+	completedAt: {
+		type: Number
+	}
+});
+
+var newTodo = new Todo({
+	text: 'Cook dinner',
+	completed: true,
+	completedAt: new Date().getTime()
+});
+
+newTodo.save()
+.then(
+	doc => {
+		console.log('Saved new todo', doc);
+	}
+)
+.catch(
+	err => {
+		console.log('Unable to save todo');
+	}
+);
