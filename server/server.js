f1ec176862ad69c003443a26e3d5c808478dc1ad
Ali Usman <ali.usman@indusvalleylabs.com>
2019-02-28 16:34:11 +0500

    Added new test case for GET /todos

--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ app.post('/todos', (req, res) => {
 		doc => res.send(doc),
 		err => res.status(400).send(err)
 	);
-})
+});
 
 app.get('/todos', (req, res) => {
 	Todo.
 	find()
 	.then(todos => res.send({todos}))
 	.catch(e => res.status(400).send(e));
-})
+});
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
