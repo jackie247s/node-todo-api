8b2b71f848133be4c82d079fbf8aa7e4b0f4cf5e
Ali Usman <ali.usman@indusvalleylabs.com>
2019-02-28 14:39:27 +0500

    Created todo creation api rout

--- server/server.js
+++ server/server.js
@@ -1,34 +1,24 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var mongoose = require('./db/mongoose');
+var { Todo, User } = require('./models');
 
-var Todo = mongoose.model('Todo', {
-	text: {
-		type: String
-	},
-	completed: {
-		type: Boolean
-	},
-	completedAt: {
-		type: Number
-	}
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then(
+		doc => res.send(doc),
+		err => res.status(400).send(err)
+	);
+})
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
 });
-
-var newTodo = new Todo({
-	text: 'Cook dinner',
-	completed: true,
-	completedAt: new Date().getTime()
-});
-
-newTodo.save()
-.then(
-	doc => {
-		console.log('Saved new todo', doc);
-	}
-)
-.catch(
-	err => {
-		console.log('Unable to save todo');
-	}
-);
