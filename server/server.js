2f481b4000362ee983aeec66c2a9c4b8ef933cbf
Ali Usman <ali@pumpjackdataworks.com>
2019-07-15 19:09:06 +0500

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
+var _ = require('lodash');
 
 var mongoose = require('./db/mongoose');
 var { Todo, User } = require('./models');
@@ -42,8 +43,44 @@ app.get('/todos/:id', (req, res) => {
 
 		res.send({todo});
 	})
-	.catch(e => res.status(400).send());
-})
+	.catch(e => res.status(400).send(e));
+});
+
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.copmletedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true},).then(todo => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({ todo });
+	}).catch(e => {
+		res.status(400).send();
+	});
+});
+
+app.post('/users', (req, res) => {
+	const userData = _.pick(req.body, ['email', 'password']);
+	const user = new User(userData);
+
+	user.save()
+	.then(doc => user.generateAuthToken())
+	.then((token) => res.header('x-auth', token).send(user))
+	.catch(err => res.status(400).send(err));
+});
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
